e5032549944df6978f7c51417ecf7bf23968a928
Todey <chloe.e.hanks@gmail.com>
2023-03-14 15:50:15 -0500

    updated about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="page-container">
-      <h1>About Me</h1>
-      <p>
-      After 5 years in the aerospace industry working as a technical hardware lead on NASA's new spacesuit development and, later, developing science instruments for other NASA and NASA-affiliated entities, Chloe looks forward to balancing her skillset with software experience.
-      </p>
-
-      <p>
-      Chloe graduated with a BSEE and BA Russian Language in 2017. She enjoys urban sketching; oil painting; creating with textiles, reading, and writing.
-      </p>
+    <div className="display-flex row page-container">
+      <div className='col'>
+        <h1>About Me</h1>
+        <p>
+        After 5 years in the aerospace industry working as a technical hardware lead on NASA's new spacesuit development and, later, developing science instruments for other NASA and NASA-affiliated entities, Chloe looks forward to balancing her skillset with software experience.
+        </p>
+        <p>
+        Chloe graduated with a BSEE and BA Russian Language in 2017. She enjoys urban sketching; oil painting; creating with textiles, reading, and writing.
+        </p>
+      </div>
+      <div className='col'>
+        <img src="images/monchouchou.png" alt="profile img of Chloe"></img>
+      </div>
     </div>
   );
 }
