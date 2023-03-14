cb856fb2cc89651a99fecfad4cfdad05bd9e6259
Todey <chloe.e.hanks@gmail.com>
2023-03-14 10:01:38 -0500

    copied and pasted activity 20>24 src into project folder. There was an issue with rendering. Assuming it's from the newer version conflicting with my code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer";
 
-import Header from './components/Header';
-import Navigator from './components/Navigator';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div id="about" className='col'>
-      <h1>About Me</h1>
+    <div>
+      <h1>About Page</h1>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
