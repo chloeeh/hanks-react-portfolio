a912d928e7f656b392741f4cb3ebfc20d40cf7fe
Todey <chloe.e.hanks@gmail.com>
2023-03-14 01:54:30 -0500

    replaced v18 reactDOM... with ReactDOM.render in ./src/index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//     document.getElementById('root')
+// );
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-    document.getElementById('root')
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
