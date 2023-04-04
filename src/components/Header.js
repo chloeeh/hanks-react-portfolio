40ff5379836e435deeda7c5e5a1a11d647338f93
Todey <chloe.e.hanks@gmail.com>
2023-04-04 13:22:31 -0500

    fixed header href to #aboutme

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import '../App.css';
 export default function Header() {
     return (
         <div id="header" className='header'>
-            <a className='navbar-brand ' href="about">
+            <a className='navbar-brand ' href="#aboutme">
 
                 <h1 className='flex justify-content-center align-center m-auto pt-3'>Chloe Hanks</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
